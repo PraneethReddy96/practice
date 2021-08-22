75142d92bf7b48b8977b456d83cdce8dd1973a2e
DineshReddyP <praneeth.t96@gmail.com>
2021-08-22 23:02:34 +0530

    practice

--- apps/app/src/main/java/com/praneeth/frags/Fragment3.kt
+++ apps/app/src/main/java/com/praneeth/frags/Fragment3.kt
@@ -8,22 +8,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.praneeth.frags.R
 
+// TODO: Rename parameter arguments, choose names that match
 
-class SecondFragment : Fragment(R.layout.fragment_second) {
+class Fragment3 : Fragment(R.layout.fragment_3) {
 
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        Log.d(ContentValues.TAG, "onAttach1")
+        Log.d(ContentValues.TAG, "onAttach2")
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(ContentValues.TAG, "onCreate1")
+        Log.d(ContentValues.TAG, "onCreate2")
     }
 
 
@@ -32,50 +31,48 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d(ContentValues.TAG, "onCreateView1")
+        Log.d(ContentValues.TAG, "onCreateView2")
         return super.onCreateView(inflater, container, savedInstanceState)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(ContentValues.TAG, "onViewCreated1")
+        Log.d(ContentValues.TAG, "onViewCreated2")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(ContentValues.TAG, "onStart1")
+        Log.d(ContentValues.TAG, "onStart2")
     }
     override fun onResume() {
         super.onResume()
-        Log.d(ContentValues.TAG, "onResume1")
+        Log.d(ContentValues.TAG, "onResume2")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(ContentValues.TAG, "onPause1")
+        Log.d(ContentValues.TAG, "onPause2")
     }
 
 
     override fun onStop() {
         super.onStop()
-        Log.d(ContentValues.TAG, "onStop1")
+        Log.d(ContentValues.TAG, "onStop2")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        Log.d(ContentValues.TAG, "onDestroyView1")
+        Log.d(ContentValues.TAG, "onDestroyView2")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(ContentValues.TAG, "onDestroy1")
+        Log.d(ContentValues.TAG, "onDestroy2")
     }
 
 
     override fun onDetach() {
         super.onDetach()
-        Log.d(ContentValues.TAG, "onDetach1")
+        Log.d(ContentValues.TAG, "onDetach2")
     }
 
 }
-
-
--- apps/app/src/main/java/com/praneeth/frags/MainActivity.kt
+++ apps/app/src/main/java/com/praneeth/frags/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.praneeth.myapplication
+package com.praneeth.frags
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -20,18 +22,34 @@ class MainActivity : AppCompatActivity() {
         val fm = supportFragmentManager
 
         findViewById<FrameLayout>(R.id.flContainer1)
+       var button= findViewById<Button>(R.id.btn)
+       var button1= findViewById<Button>(R.id.btn1)
         findViewById<FrameLayout>(R.id.flContainer2)
+
+
+        button.setOnClickListener(View.OnClickListener {
+            val ft1: FragmentTransaction = fm.beginTransaction()
+            val fragment2 = SecondFragment()
+            ft1.replace(R.id.flContainer1, fragment2,"myFragmentTag")
+            ft1.commit()
+
+
+        })
+
+        button1.setOnClickListener(View.OnClickListener {
+            val ft3: FragmentTransaction = fm.beginTransaction()
+            val fragment3 = Fragment3()
+            ft3.replace(R.id.flContainer1, fragment3,"myFragmentTag1")
+            ft3.commit()
+
+
+        })
+
 
             val ft: FragmentTransaction = fm.beginTransaction()
             val fragment = FirstFragment()
             ft.add(R.id.flContainer1, fragment,"myFragmentTag")
             ft.commit()
-
-           val ft1: FragmentTransaction = fm.beginTransaction()
-           val fragment2 = SecondFragment()
-           ft1.add(R.id.flContainer2, fragment2,"myFragmentTag")
-           ft1.commit()
-
 
 
     }
